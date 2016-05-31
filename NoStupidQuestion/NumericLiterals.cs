3e26da8b280b5c6a29ea283898ed0f2ff751a422
sam lin <evaoosyz@gmail.com>
2016-05-31 23:12:54 +0800

    整理code

--- NoStupidQuestion/NumericLiterals.cs
+++ NoStupidQuestion/NumericLiterals.cs
@@ -14,19 +14,29 @@ namespace NoStupidQuestion
             //https://dotblogs.com.tw/hatelove/archive/2012/04/20/var-and-anonymous-types-introduction.aspx
 
             var n1 = 10;    //int       Int32     
-            var n3 = 10F;   //float     Single
-            var n4 = 10D;   //double    Double
-            var n5 = 10M;   //decimal   Decimal
-            var n6 = 10U;   //uint      UInt32
-            var n7 = 10L;   //long      Int64
-            var n8 = 10UL;  //ulong     UInt64
+            var n2 = 10F;   //float     Single
+            var n3 = 10D;   //double    Double
+            var n4 = 10M;   //decimal   Decimal
+            var n5 = 10U;   //uint      UInt32
+            var n6 = 10L;   //long      Int64
+            var n7 = 10UL;  //ulong     UInt64
 
-            var b = 50 / 100;
-            Console.WriteLine(b);
-            var c = 50 / (100 * 1.0);
-            Console.WriteLine(c);
-            var e = 50 / (100D);
-            Console.WriteLine(e);
+            var temp1 = 50 / 100;
+            Console.WriteLine(temp1);
+
+            var temp2 = 50 / (100 * 1.0);
+            Console.WriteLine(temp2);
+
+            var temp3 = 50 / (100D);
+            Console.WriteLine(temp3);
+
+            n1.ToString();
+            n2.ToString();
+            n3.ToString();
+            n4.ToString();
+            n5.ToString();
+            n6.ToString();
+            n7.ToString();
         }
     }
 }
--- NoStupidQuestion/Program.cs
+++ NoStupidQuestion/Program.cs
@@ -8,6 +8,9 @@ namespace NoStupidQuestion
 {
     class Program
     {
+        //沒有蠢問題系列 的由來
+        //該怎麼提高上課的學習效果？
+        //https://dotblogs.com.tw/hatelove/2016/04/27/learn-to-ask-and-answer-questions
         static void Main(string[] args)
         {
             NumericLiterals.Suffix();
